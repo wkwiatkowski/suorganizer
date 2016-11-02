aba877cd7262985ed13febdc70ce9ec00b98acf4
Waldemar Kwiatkowski <wkwiatkowski@hindukusz.com>
2016-11-02 16:35:25 +0100

    Modele - uzupelnione o detale

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,32 @@
 from django.db import models
+from django.utils import timezone
+
+from organizer.models import Startup, Tag
 
 # Create your models here.
 
 class Post(models.Model):
-    pass
+    title = models.CharField(
+        max_length=31,
+        db_index=True)
+    slug = models.SlugField(
+        max_length=61,
+#        unique_for_month='pub_date',
+        help_text="A label for URL config.")
+    text = models.TextField()
+#    pub_date = models.DateField()
+    tags = models.ManyToManyField(
+        Tag,
+        related_name='blog_post')
+    startups = models.ManyToManyField(
+        Startup,
+        related_name='blog_post')
+    def __str__(self):
+        return "{} on {}".format(
+            self.title,
+            self.pub_date.strftime('%Y-%m-%d'))
+    class Meta:
+        verbose_name = 'blog post'
+#        ordering = ['-pub_date', 'title']
+#        get_latest_by = 'pub_date'
+
--- organizer/models.py
+++ organizer/models.py
@@ -2,13 +2,52 @@ from django.db import models
 
 # Create your models here.
 
-class Startup(models.Model):
-    pass
-
 class Tag(models.Model):
-    pass
+    name = models.SlugField(
+        max_length=31,
+        unique=True)
+    slug = models.SlugField(
+        max_length=61,
+        unique=True,
+        help_text='A label for URL config')
+    def __str__(self):
+        return self.name
+    class Meta:
+        ordering = ['name']
+
+class Startup(models.Model):
+    name = models.CharField(
+        max_length=31,
+        db_index=True)
+    slug = models.SlugField(
+        max_length=61,
+        unique=True,
+        help_text='A label for URL config')
+    description = models.TextField()
+    founded_date = models.DateField('date founded')
+    contact = models.EmailField()
+    website = models.URLField(max_length=255)
+    tags = models.ManyToManyField(Tag)
+    def __str__(self):
+        return self.name
+    class Meta:
+        ordering = ['name']
+        get_latest_by = 'founded_date'
+
 
 class NewsLink(models.Model):
-    pass
+    title = models.CharField(max_length=63)
+    pub_date = models.DateField('published date')
+    link = models.URLField(max_length=255)
+    startup = models.ForeignKey(Startup)
+    def __str__(self):
+        return "{}:{}".format(
+            self.startup, self.title)
+    class Meta:
+        verbose_name = 'news article'
+        ordering = ['-pub_date']
+        get_latest_by = 'pub_date'
+
+
 
 
