8ec9fbe29294f53c692839dcad6b3a8ee3471c89
Waldemar Kwiatkowski <wkwiatkowski@hindukusz.com>
2016-11-02 14:55:10 +0100

    poczatek-struktura projektu

---
+++ organizer/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class Startup(models.Model):
+    pass
+
+class Tag(models.Model):
+    pass
+
+class NewsLink(models.Model):
+    pass
+
+
